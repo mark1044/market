38ab775f75ca430c307613264cf6836bbd93bd03
M. Melendez <mark.st.melendez@gmail.com>
2017-02-04 13:57:26 -0500

    Merge branch 'master' of https://github.com/mark1044/market
    # Conflicts:
    #	src/main/resources/templates/layouts/footer.html

--- src/main/resources/static/JS/portal.js
+++ src/main/resources/static/JS/portal.js
@@ -15,15 +15,33 @@
 var app = angular.module('marketApp', []);
 
 
+
 /*
  marketControl refers to the controller with the ng-controller directive
- */
-app.controller('marketControl', function($http){
+
+app.controller('marketControl', function($scope, $http){
 
 
     $http.get('/products/JKD%20boxing%20glove').then(function(response) {
         console.log(response.data.description);
-        this.productDescription = response.data.description;
+        $scope.product = response.data;
     });
+
+});
+
+
+*/
+
+app.controller('marketControl', function($scope, $http){
+
+    $http({
+        method : "GET",
+        url : "/products/JKD%20boxing%20glove"
+    }).then(function mySucces(response) {
+        $scope.product = response.data;
+    }, function myError(response) {
+        $scope.product = response.statusText;
+    });
+
 
 });
